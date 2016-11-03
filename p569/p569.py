28fdb7decb51e00b54b5757933169c0f11a35b31
Benjamin Pheil <benpheil@gmail.com>
2016-11-02 20:15:57 -0400

    Add a test for brute force algo.

--- p569/p569.py
+++ p569/p569.py
@@ -29,15 +29,12 @@ def mountains_and_valleys_to(k_max, max_prime):
         yield (distance, elevation)
 
 
-def compute_visible_peaks(skyline):
+def compute_visible_peaks_brute(skyline):
     peaks = [skyline[2*k] for k in range(0, len(skyline) // 2)]
     n_peaks = len(peaks)
-    visible_peaks = numpy.matrix(numpy.zeros((n_peaks, n_peaks)))
+    visible_peaks = numpy.matrix(numpy.zeros((n_peaks, n_peaks)), dtype=int)
     for k in range(1, len(peaks)):
-        print("Looking back from {}".format(k + 1))
-        print(visible_peaks)
         for j in range(0, k):
-            print("    Checking to see if I can see {}".format(j + 1))
             rise = float(peaks[k][1] - peaks[j][1])
             run = float(peaks[k][0] - peaks[j][0])
             slope = rise / run
@@ -49,31 +46,43 @@ def compute_visible_peaks(skyline):
                 los_at_l = peaks[j][1] + dist_from_j * slope
                 if los_at_l <= peaks[l][1]:
                     blocked = True
-                    print("    blocked by {}".format(l + 1))
                     break
             if not blocked:
-                print("    can see it.")
                 visible_peaks[j, k] = 1
     return visible_peaks
 
+def test_compute_visible_peaks_brute():
+    max_k = 9
+    max_prime = int(100)
+    skyline = list(mountains_and_valleys_to(max_k, max_prime))
+    visible_peaks = compute_visible_peaks_brute(skyline)
+    visibility_count = numpy.sum(visible_peaks, axis=0)
+    assert(visibility_count[0, 0] == 0)
+    assert(visibility_count[0, 1] == 1)
+    assert(visibility_count[0, 2] == 1)
+    assert(visibility_count[0, 8] == 3)
+
 def run_tests():
     test_primes()
+    test_compute_visible_peaks_brute()
 
 def main():
-    max_k = 100000
+    max_k = 9
     max_prime = int(1e4)
 
     skyline = list(mountains_and_valleys_to(max_k, max_prime))
-    visible_peaks = compute_visible_peaks(skyline)
+    visible_peaks = compute_visible_peaks_brute(skyline)
     print(visible_peaks)
-    skyline_arr = list(list(t for t in zip(*skyline)))
+    visibility_count = numpy.sum(visible_peaks, axis=0)[0]
+    print(visibility_count[0:max_k])
 
+    #skyline_arr = list(list(t for t in zip(*skyline)))
     #pyplot.plot(skyline_arr[0], skyline_arr[1])
     #pyplot.axis('equal')
 
-    pyplot.imshow(visible_peaks, interpolation='none')
+    #pyplot.imshow(visible_peaks, interpolation='none')
 
-    pyplot.show()
+    #pyplot.show()
 
 if __name__ == '__main__':
     run_tests()
